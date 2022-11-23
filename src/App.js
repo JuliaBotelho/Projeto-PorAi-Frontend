2bbb35424250698037081d4b5daf162dcb6746a3
Julia Botelho <cajujumo@gmail.com>
2022-11-23 15:38:49 -0300

    feat: addded GlobalStyle with ResetCSS

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import AuthProvider from "./contextelements/auth.js"
+import GlobalStyle from "./GlobalStyle.js";
 
 function App() {
   return (
